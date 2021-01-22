465be7e402d2d651185e3fa4e78149c685e02bcf
quietbluelake <supernovaswirl96@gmail.com>
2021-01-22 16:06:59 +0000

    TEST(app): Test workflow with ChromeHeadless Switch back from Firefox to Chrome and add ChromeHeadless as an option

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
