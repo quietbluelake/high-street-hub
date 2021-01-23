5b60739a25aa7d6913b9c70a7cfd1f0f9d10a481
quietbluelake <supernovaswirl96@gmail.com>
2021-01-23 18:13:52 +0000

    SETUP(DashboardComponent): Write unit test stubs Follow TDD procedures and begin by writing unit tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -22,4 +22,52 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should be responsive', () => {
+    expect(true).toEqual(true);
+  });
+  it('should have black background', () => {
+    expect(true).toEqual(true);
+  });
+  it('should display maximum of 10 cards', () => {
+    expect(true).toEqual(true);
+  });
+  it('cards should be draggable', () => {
+    expect(true).toEqual(true);
+  });
+  it('cards should automatically sort when dragged', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to edit a card in place', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to filter a graph/chart in edit mode', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to change colours of graphs/charts in edit mode', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to add notes in edit mode', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to import data in edit mode to update stats', () => {
+    expect(true).toEqual(true);
+  });
+  it('import should make a POST request to correct stat type', () => {
+    expect(true).toEqual(true);
+  });
+  it('import should update data in real-time', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be making the correct GET requests per card', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to resize cards to a min and max size', () => {
+    expect(true).toEqual(true);
+  });
+  it('should autosave after leaving edit mode', () => {
+    expect(true).toEqual(true);
+  });
+  it('should be able to exit back to Explorer', () => {
+    expect(true).toEqual(true);
+  });
 });
