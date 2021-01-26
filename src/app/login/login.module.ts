c80162c98f32306a88715c999f44d4dab455115e
quietbluelake <supernovaswirl96@gmail.com>
2021-01-26 23:46:59 +0000

    ADD(LoginComponent): UI setup Begin developing the UI with a simple login form

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,12 +5,23 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [LoginComponent, UserAuthenticationComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
