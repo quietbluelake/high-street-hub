d2c93674b87d12963e2e223ccf0efa62b2b35e60
quietbluelake <supernovaswirl96@gmail.com>
2021-01-31 16:00:50 +0000

    FIX(app): Add HttpClientTestingModule to tests Add HttpClientTestingModule to TestBed configuration for each service

--- src/app/_services/authentication.service.spec.ts
+++ src/app/_services/authentication.service.spec.ts
@@ -2,12 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
 
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+// Other imports
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AuthenticationService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
--- src/app/_services/user.service.spec.ts
+++ src/app/_services/user.service.spec.ts
@@ -2,12 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+// Other imports
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 describe('UserService', () => {
   let service: UserService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(UserService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
